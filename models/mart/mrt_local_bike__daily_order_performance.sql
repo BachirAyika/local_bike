a4f196c1216b3c5b25bf105e30d0cd51fa3486cf
Bachir <chris.ayika96@gmail.com>
2025-06-02 19:00:07 +0200

    Adding mart level transformation

---
+++ models/mart/mrt_local_bike__daily_order_performance.sql
@@ -0,0 +1,10 @@
+select order_date,
+    city,
+    state,
+    COUNT(order_id) as total_orders,
+    SUM(total_order_amount) as total_order_amount,
+    AVG(total_order_amount) as avg_order_amount
+from {{ ref('int_local_bike_sales__orders') }} 
+group by order_date,
+    city,
+    state
